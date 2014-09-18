86ad0922741b5f610aa786a3f47b149834be9fd1
ttitto <ttitto@users.noreply.github.com>
2014-09-18 23:52:07 +0300

    task 5 solved

---
+++ Softuni/StaticMembersHW/BitArray/BitArray.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArray
+{
+   public class BitArray
+    {
+       private bool[] arr;
+       private int size;
+
+       public BitArray(int size){
+           this.Size = size;
+           this.Arr = new bool[size];
+       }
+
+       public bool[] Arr
+       {
+           get { return this.arr; }
+           set {
+               if (null == value) throw new ArgumentNullException();
+               this.arr = value; }
+       }
+       public int Size
+       {
+           get { return this.size; }
+           set
+           {
+               if (value < 0) throw new ArgumentException("Invalid value for the array size");
+               this.size= value;
+           }
+       }
+
+       public bool this[int index]
+       {
+           get {
+               if (index < 0 || index > 100000) throw new ArgumentException("The given index is outside the allowed range [0 - 100000]");
+               return this.Arr[index]; }
+           set {
+               if (index < 0 || index> 100000) throw new ArgumentException("The given index is outside the allowed range [0 - 100000]");
+               if (value != false && value != true) throw new ArgumentException("The given value is not a valid bit value");
+               this.Arr[index] = value;
+           }
+       }
+       /// <summary>
+       /// Place ones at the positions passed as arguments
+       /// </summary>
+       /// <param name="positions"></param>
+       public void SetBits(params int[] positions)
+       {
+           for (int i = 0; i < positions.Length; i++)
+           {
+               this[positions[i]] = true;
+           }
+       }
+
+       public override string ToString()
+       {
+           return ArrayToBigNumber().ToString();
+       }
+
+       private BigInteger ArrayToBigNumber()
+       {
+           BigInteger num = BigInteger.Zero;
+           BigInteger exp2 = BigInteger.One;
+           for (int pos = 0; pos < this.Size; pos++)
+           {
+               exp2 *= 2;
+               if (this[pos])
+               {
+                   num += exp2/2;
+               }
+           }
+
+           return num;
+       }
+      
+    }
+}
---
+++ Softuni/StaticMembersHW/BitArray/BitArrayApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArray
+{
+    class BitArrayApp
+    {
+        static void Main(string[] args)
+        {
+            BitArray myBitArr = new BitArray(5);
+            myBitArr.SetBits(2, 4);
+            Console.WriteLine(myBitArr.ToString());
+
+            BitArray myBitArr1 = new BitArray(16);
+            myBitArr1.SetBits(2, 4, 8);
+            Console.WriteLine(myBitArr1.ToString());
+
+            BitArray myBitArr2 = new BitArray(100001);
+            myBitArr2.SetBits(2, 4, 8);
+            Console.WriteLine(myBitArr2.ToString());
+
+            BitArray myBitArr3 = new BitArray(64);
+            myBitArr3.SetBits(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,
+                39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62);
+            Console.WriteLine(myBitArr3.ToString());
+
+            BitArray myBitArr4 = new BitArray(100001);
+            myBitArr4.SetBits(2, 4, 8,100001); //ArgumentException
+            Console.WriteLine(myBitArr4.ToString());
+        }
+    }
+}

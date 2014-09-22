1219cb73e3b8fb51d7990a18dd52ed851d1b3ed9
ttitto <ttitto@users.noreply.github.com>
2014-09-22 12:40:02 +0300

    tasks 1-2 solved

---
+++ Softuni/ExceptionHandlingHW/EnterNumbers/EnterNumbersClass.cs
@@ -0,0 +1,48 @@
+﻿namespace EnterNumbers
+{
+    using System;
+
+    public class EnterNumbersClass
+    {
+        public static void Main()
+        {
+            int counter = 0;
+            int start = 1;
+
+            while (counter < 10)
+            {
+                try
+                {
+                    int currentNum = ReadNumbers(start, 100);
+
+                    if (currentNum > start)
+                    {
+                        start = currentNum;
+                    }
+
+                    counter++;
+                }
+                catch (FormatException fex)
+                {
+                    Console.WriteLine("{0} Repeat input!", fex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("{0} Repeat input!", ex.Message);
+                }
+            }
+        }
+
+        public static int ReadNumbers(int start, int end)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num <= start || num > end)
+            {
+                throw new ArgumentException("The number is outside of the requested range!");
+            }
+
+            return num;
+        }
+    }
+}
---
+++ Softuni/ExceptionHandlingHW/SquareRoot/SquareRootClass.cs
@@ -0,0 +1,39 @@
+﻿namespace SquareRoot
+{
+    using System;
+
+    public class SquareRootClass
+    {
+        public static void Main()
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                double result = Math.Sqrt(num);
+
+                Console.WriteLine(result);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.Error.WriteLine("Invalid number!");
+            }
+            catch (FormatException)
+            {
+                Console.Error.WriteLine("Invalid number!");
+            }
+            catch (OverflowException)
+            {
+                Console.Error.WriteLine("Invalid number!");
+            }
+            catch (Exception)
+            {
+                Console.Error.WriteLine("Square root calculation error!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}

519c367f91de48e9523ba81ed27a88227f06d207
Todor Todorov <ttitto_prog@vfemail.net>
2017-01-15 12:27:44 +0200

    PreferImmutableObjects: initial commit; unexpected change of the cost from the Reserve method for next buys

---
+++ PreferImmutableObjects/PreferImmutableObjects/MoneyAmount.cs
@@ -0,0 +1,13 @@
+﻿namespace PreferImmutableObjects
+{
+    internal class MoneyAmount
+    {
+        public decimal Amount { get; set; }
+        public string CurrencySymbol { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Amount} {this.CurrencySymbol}";
+        }
+    }
+}
---
+++ PreferImmutableObjects/PreferImmutableObjects/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PreferImmutableObjects
+{
+    class Program
+    {
+        static bool IsHappyHour { get; set; }
+
+        static void Reserve(MoneyAmount cost)
+        {
+            if (IsHappyHour)
+            {
+                cost.Amount *= .5M;
+            }
+
+            Console.WriteLine("\nReserving an item that costs {0}", cost);
+        }
+
+        static void Buy(MoneyAmount wallet, MoneyAmount cost)
+        {
+            bool enounghMoney = wallet.Amount >= cost.Amount;
+            Reserve(cost);
+            if (enounghMoney)
+            {
+                Console.WriteLine("You will pay {0} with your {1}", cost, wallet);
+            }
+            else
+            {
+                Console.WriteLine("You cannot pay {0} with your {1}", cost, wallet);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Buy(new MoneyAmount() { Amount = 12, CurrencySymbol = "USD" },
+                new MoneyAmount() { Amount = 10, CurrencySymbol = "USD" });
+            Buy(new MoneyAmount() { Amount = 7, CurrencySymbol = "USD" },
+                new MoneyAmount() { Amount = 10, CurrencySymbol = "USD" });
+
+            IsHappyHour = true;
+
+            Buy(new MoneyAmount() { Amount = 7, CurrencySymbol = "USD" },
+                new MoneyAmount() { Amount = 10, CurrencySymbol = "USD" });
+
+            Console.ReadLine();
+        }
+    }
+}

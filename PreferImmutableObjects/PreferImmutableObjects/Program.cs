7195cf64b342e037fa4f02d499475bf5c8d306a0
Todor Todorov <ttitto_prog@vfemail.net>
2017-01-15 12:56:15 +0200

    PreferImmutableObjects: aliasing bug fixed in a native way

--- PreferImmutableObjects/PreferImmutableObjects/Program.cs
+++ PreferImmutableObjects/PreferImmutableObjects/Program.cs
@@ -6,23 +6,39 @@ namespace PreferImmutableObjects
     {
         static bool IsHappyHour { get; set; }
 
-        static void Reserve(MoneyAmount cost)
+        static MoneyAmount Reserve(MoneyAmount cost)
         {
+            MoneyAmount newCost;
             if (IsHappyHour)
             {
-                cost.Amount *= .5M;
+                newCost = new MoneyAmount()
+                {
+                    Amount = cost.Amount * 0.5M,
+                    CurrencySymbol = cost.CurrencySymbol
+                };
+            }
+            else
+            {
+                newCost = cost;
             }
 
             Console.WriteLine("\nReserving an item that costs {0}", cost);
+            return newCost;
         }
 
         static void Buy(MoneyAmount wallet, MoneyAmount cost)
         {
             bool enounghMoney = wallet.Amount >= cost.Amount;
-            Reserve(cost);
-            if (enounghMoney)
+            MoneyAmount finalCost = Reserve(cost);
+            bool finalEnough = wallet.Amount >= finalCost.Amount;
+
+            if (enounghMoney  && finalEnough)
             {
                 Console.WriteLine("You will pay {0} with your {1}", cost, wallet);
+            }
+            else if (finalEnough)
+            {
+                Console.WriteLine("This time, {0} will be enough to pay{1}", wallet, finalCost);
             }
             else
             {

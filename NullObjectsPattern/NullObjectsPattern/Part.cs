9ec8ca38a7bb00db892dad189f29b0baf44ae9cd
Todor Todorov <ttitto_prog@vfemail.net>
2017-01-19 20:56:52 +0200

    OptionalObjects: Circuitry can be missing and cause NullReferenceException

---
+++ NullObjectsPattern/NullObjectsPattern/Part.cs
@@ -0,0 +1,20 @@
+﻿namespace NullObjectsPattern
+{
+    using System;
+
+    internal class Part
+    {
+        public DateTime InstallmentDate { get; }
+        public DateTime DefectDetectedOn { get; private set; }
+
+        public Part(DateTime installmentDate)
+        {
+            this.InstallmentDate = installmentDate;
+        }
+
+        public void MarkDefective(DateTime withDate)
+        {
+            this.DefectDetectedOn = withDate;
+        }
+    }
+}
--- NullObjectsPattern/NullObjectsPattern/SoldArticle.cs
+++ NullObjectsPattern/NullObjectsPattern/SoldArticle.cs
@@ -1,12 +1,15 @@
-﻿using System;
-
-namespace NullObjectsPattern
+﻿namespace NullObjectsPattern
 {
+    using System;
+
     internal class SoldArticle
     {
         public IWarranty MoneyBackGuarantee { get; private set; }
         public IWarranty ExpressWarranty { get; private set; }
         public IWarranty NotOperationalWarranty { get; }
+        private IWarranty FailedCircuitryWarranty { get; set; }
+        private Part Circuitry { get; set; }
+        private IWarranty CircuitryWarranty { get; set; }
 
         public SoldArticle(IWarranty moneyBack, IWarranty express)
         {
@@ -23,6 +26,7 @@ namespace NullObjectsPattern
             this.MoneyBackGuarantee = moneyBack;
             this.ExpressWarranty = VoidWarranty.Instance;
             this.NotOperationalWarranty = express;
+            this.CircuitryWarranty = VoidWarranty.Instance;
         }
 
         public void VisibleDamage()
@@ -34,6 +38,23 @@ namespace NullObjectsPattern
         {
             this.MoneyBackGuarantee = VoidWarranty.Instance;
             this.ExpressWarranty = this.NotOperationalWarranty;
+        }
+
+        public void InstallCircuitry(Part circuitry, IWarranty extendedWarranty)
+        {
+            this.Circuitry = circuitry;
+            this.FailedCircuitryWarranty = extendedWarranty;
+        }
+
+        public void CircuitryNotOperational(DateTime detectedOn)
+        {
+            this.Circuitry.MarkDefective(detectedOn);
+            this.CircuitryWarranty = this.FailedCircuitryWarranty;
+        }
+
+        public void ClaimCircuitryWarranty(Action onValidClaim)
+        {
+            this.CircuitryWarranty.Claim(this.Circuitry.DefectDetectedOn, onValidClaim);
         }
     }
 }

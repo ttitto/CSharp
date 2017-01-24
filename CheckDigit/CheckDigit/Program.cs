58022ab17d8de71fbc81b63505ea448cd6fb458b
Todor Todorov <ttitto_prog@vfemail.net>
2017-01-24 20:21:04 +0200

    Check digit: recognize infrastructural code and domain logic

--- CheckDigit/CheckDigit/Program.cs
+++ CheckDigit/CheckDigit/Program.cs
@@ -6,23 +6,23 @@
         {
             int sum = 0;
             bool isOddPos = true;
-            while (number > 0)
+            while (number > 0)                      // infrastructure
             {
-                int digit = (int)(number % 10);
-                if (isOddPos)
+                int digit = (int)(number % 10);     // infrastructure
+                if (isOddPos)                       // domain
                 {
-                    sum += 3 * digit;
+                    sum += 3 * digit;               // 3 = parameter
                 }
                 else
                 {
                     sum += digit;
-                    number /= 10;
-                    isOddPos = !isOddPos;
+                    number /= 10;                   // infrastructure
+                    isOddPos = !isOddPos;           // domain
                 }
             }
 
-            int modulo = sum % 7;
-            return modulo;
+            int modulo = sum % 7;                   // 7 = parameter
+            return modulo;                          // % = domain
         }
 
         static void Main(string[] args)

c797f944c6b1cf143f500d126ce178cd68c8c677
Todor Todorov <ttitto_prog@vfemail.net>
2017-01-24 10:31:52 +0200

    Check digit: initial commit unflexible algorithm with serious cognitive mapping between user requirements and programming language possibilities

---
+++ CheckDigit/CheckDigit/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CheckDigit
+{
+    class Program
+    {
+        static int GetControllDigit(long number)
+        {
+            int sum = 0;
+            bool isOddPos = true;
+            while (number > 0)
+            {
+                int digit = (int)(number % 10);
+                if (isOddPos)
+                {
+                    sum += 3 * digit;
+                }
+                else
+                {
+                    sum += digit;
+                    number /= 10;
+                    isOddPos = !isOddPos;
+                }
+            }
+
+            int modulo = sum % 7;
+            return modulo;
+        }
+
+        static void Main(string[] args)
+        {
+            int digit = GetControllDigit(12345);
+        }
+    }
+}
